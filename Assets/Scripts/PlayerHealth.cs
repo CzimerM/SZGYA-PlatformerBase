6abe3f6d13b1eb69783a09a8b64420de4313bdfc
Csizmadia Márk <kindmysticaltree@gmail.com>
2024-03-05 10:26:52 +0100

    damage indicator

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,12 +6,17 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float maxHealth = 10f;
     [SerializeField] private GameObject bloodDropsFX;
     [SerializeField] private Slider healthBar;
-
+    [SerializeField] private Image damageFlashImg;
+    [SerializeField] private float damageTime = 0.5f;
 
     private float currentHealth;
+    private bool getDamage;
+    private Color dmgColor;
 
     private void Start()
     {
+        getDamage = false;
+        dmgColor = new Color(255, 255, 255, 0.7f);
         currentHealth = maxHealth;
         healthBar.value = maxHealth;
         healthBar.maxValue = maxHealth;
@@ -19,6 +24,7 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        getDamage = true;
         currentHealth -= damage;
         healthBar.value = currentHealth;
         Instantiate(bloodDropsFX, transform.position, transform.rotation);
@@ -31,6 +37,21 @@ public class PlayerHealth : MonoBehaviour
 
     private void MakeDead()
     {
+        damageFlashImg.color = dmgColor;
+        getDamage = true;
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (getDamage)
+        {
+            damageFlashImg.color = dmgColor;
+            getDamage = false;
+        }
+        else
+        {
+            damageFlashImg.color = Color.Lerp(damageFlashImg.color, new Color(255f,255f,255f,0f), damageTime * Time.deltaTime);
+        }
     }
 }
