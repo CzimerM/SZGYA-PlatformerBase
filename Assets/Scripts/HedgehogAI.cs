fb65a278a65140701cc280b5b86c77924d0f3ba1
Csizmadia Márk <airamek@teamorange.hu>
2024-04-09 09:32:02 +0200

    HedgehogAI fut

--- Assets/Scripts/HedgehogAI.cs
+++ Assets/Scripts/HedgehogAI.cs
@@ -4,15 +4,18 @@ public class HedgehogAI : MonoBehaviour
 {
     [SerializeField] float runSpeed = 7f;
     [SerializeField] GameObject graphics;
+    [SerializeField] private float chargeDelay = 0.5f;
+    [SerializeField] private float flipTime = 2f;
 
     private Animator animator;
     private Rigidbody2D rigidbody2d;
 
-    private bool facintgRight = false;
+    private bool facingRight = false;
     private bool canFlip = true;
-    private float flipTime = 3f;
-    private float flipChance = 0f;
-    private float chargeTime = 2f;
+    
+    private float nextFlip = 0f;
+    private float startChargeTime = 0f;
+    
 
     private bool isCharging = false;
 
@@ -25,20 +28,52 @@ public class HedgehogAI : MonoBehaviour
 
     private void Update()
     {
-        if (Time.time > flipChance)
+        if (canFlip && Time.time > nextFlip)
         {
             if (Random.Range(0, 10) >= 5) FlipFacing();
-            flipChance = Time.time + flipTime;
+            nextFlip = Time.time + flipTime;
         }
+
+        animator.SetBool("isCharging", isCharging);
     }
 
     private void FlipFacing()
     {
-        if (!canFlip) return;
+        facingRight = !facingRight;
         graphics.transform.localScale =
             new Vector3(
                 graphics.transform.localScale.x * -1,
                 graphics.transform.localScale.y,
                 graphics.transform.localScale.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (facingRight != other.transform.position.x > transform.position.x)
+                FlipFacing();
+            canFlip = false;
+            isCharging = true;
+            startChargeTime = Time.time + chargeDelay;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canFlip = true;
+            isCharging = false;
+            rigidbody2d.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (isCharging && other.CompareTag("Player") && Time.time > startChargeTime)
+        {
+            rigidbody2d.AddForce((facingRight ? Vector3.right : Vector3.left) * runSpeed);
+
+        }
     }
 }
